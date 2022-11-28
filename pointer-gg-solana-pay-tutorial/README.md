d2d168b0de0a1153a152d83f447695f75105ed38
Fyfy Dev <105049869+stream-protocol@users.noreply.github.com>
2022-11-28 12:25:42 +0200

    Fist commit

---
+++ pointer-gg-solana-pay-tutorial/README.md
@@ -0,0 +1,11 @@
+# Take payments IRL with Solana Pay
+Code for [Pointer Solana Pay Tutorial](pointer.gg/tutorials/solana-pay-irl-payments)
+
+## Original Code
+This programs copy from [pointer-gg/solana-pay-tutorial](https://github.com/pointer-gg/solana-pay-tutorial)
+
+## Run
+```
+% npm i
+% npm run dev
+```
---
+++ pointer-gg-solana-pay-tutorial/components/Footer.tsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="flex w-full justify-center border-t-2 border-gray-900 py-12 h-10">
+      <a
+        href='https://www.pointer.gg?utm_source=solana-pay'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        Point Of Sale{" "}
+        <span>Pointer</span>
+      </a>
+    </footer>
+  )
+}
